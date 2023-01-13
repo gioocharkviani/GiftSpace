de0718776eb31d1b09fec3362abfe8af36892436
gioocharkviani <gioocharkviani@gmail.com>
2023-01-13 15:34:21 +0400

    change style with js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const boxAstylechanger = document.querySelector(".main-page-boxA");
+const boxBstylechanger = document.querySelector(".main-page-boxB");
+const sideA = document.querySelector(".sideA");
+const sideB = document.querySelector(".sideB");
+const CenterBorder = document.querySelector(".Center-Border");
 
 
 boxAstylechanger.addEventListener('click' , (e) =>{
-    console.log('e');
+    sideA.style = 'width:100%';
+    sideB.style = 'width:0%';
+    CenterBorder.style = 'display:none ';
+    setTimeout(() => {
+        sideB.style = 'display:none ';
+        CenterBorder.style = 'display:none ';
+      }, "1000")
+})
+
+boxBstylechanger.addEventListener('click' , (e) =>{
+    sideB.style = 'width:100%';
+    sideA.style = 'width:0%';
+    CenterBorder.style = 'display:none ';
+    setTimeout(() => {
+        sideA.style = 'display:none ';
+      }, "1000")
 })
